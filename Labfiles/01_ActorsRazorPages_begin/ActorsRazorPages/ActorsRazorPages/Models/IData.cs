a001dd50302d984c9ef99130ae074a1e8e6d6f26
Jeff <jm@jeffz.com>
2020-06-03 03:41:53 -0400

    Got though Mod01-Ex01-Task01-Step21

---
+++ Labfiles/01_ActorsRazorPages_begin/ActorsRazorPages/ActorsRazorPages/Models/IData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActorsRazorPages.Models
+{
+    public interface IData
+    {
+        public List<Actor> ActorsList { get; set; }
+
+        public List<Actor> ActorsInitializeData();
+        public Actor GetActorById(int? id);
+
+    }
+}
